841c14965f8ccc1b9a2d52f2d845ae6f7c0cbff2
Jason Gray <jasongray@Jasons-MacBook-Pro.local>
2024-07-28 02:41:11 -0700

    fixing deployment keys

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ chkconfig docker on
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 # Clone the GitHub repo and run the Flask app
+echo "${deploy_key}" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-keyscan github.com >> /root/.ssh/known_hosts
 git clone git@github.com:grayjason1970/aws_deploy.git /home/ec2-user/app
 cd /home/ec2-user/app
 docker-compose up -d
