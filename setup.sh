9c0df72f4e1c2610ac7826e2c1c6823538587314
Jason Gray <jasongray@Jasons-MacBook-Pro.local>
2024-07-28 10:22:32 -0700

    updated setup.sh to ensure packages are present.  Included docker-compose.yml file.  Cleaned up main.tf file

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-yum update -y
-yum install -y git docker
-service docker start
-usermod -a -G docker ec2-user
-chkconfig docker on
+sudo yum update -y
+sudo yum install -y gcc
+sudo yum install -y python3
+sudo yum install -y python3-devel
+sudo yum install -y mariadb
+sudo yum install -y mariadb-devel
+sudo yum install -y pip3
+sudo pip3 install flask sqlalchemy flask_sqlalchemy boto3 pymysql mysqlclient
+sudo yum install -y git docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker
 # Install Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 # Clone the GitHub repo and run the Flask app
-echo "${deploy_key}" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-keyscan github.com >> /root/.ssh/known_hosts
+ssh-keyscan github.com >> /home/ec2-user/.ssh/known_hosts
 git clone git@github.com:grayjason1970/aws_deploy.git /home/ec2-user/app
 cd /home/ec2-user/app
 docker-compose up -d
