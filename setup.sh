2d5810de3bcd9d1f9b0a04199e600fbc919ae558
Jason Gray <jasongray@Jasons-MacBook-Pro.local>
2024-07-28 02:17:02 -0700

    adding secret deploy key

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ chkconfig docker on
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 # Clone the GitHub repo and run the Flask app
-git clone https://github.com/your-username/your-repo.git /home/ec2-user/app
+git clone git@github.com:grayjason1970/aws_deploy.git /home/ec2-user/app
 cd /home/ec2-user/app
 docker-compose up -d
