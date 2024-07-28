716b9ce83428bf56bc1b94137b7eebf526333406
Jason Gray <jasongray@Jasons-MacBook-Pro.local>
2024-07-28 14:18:11 -0700

    ensure ssh keys are deployed via github actions

--- main.tf
+++ main.tf
@@ -19,3 +19,7 @@ resource "aws_instance" "flask_app" {
 output "ec2_instance_public_ip" {
   value = aws_instance.flask_app.public_ip
 }
+
+output "ec2_instance_id" {
+  value = aws_instance.flask_app.id
+}
