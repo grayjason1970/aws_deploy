13904215be1d116289c9ef11b69a6574ee399e31
Jason Gray <jasongray@Jasons-MacBook-Pro.local>
2024-07-28 01:48:17 -0700

    rebuilt variables and main.tf

--- main.tf
+++ main.tf
@@ -2,45 +2,24 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "flask_sg" {
-  name_prefix = "flask_sg_"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "flask_sg"
-  }
-}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "security_group_id" {}
+variable "key_name" {}
+variable "instance_type" { default = "t2.micro" }
+variable "ami_id" { default = "ami-0c55b159cbfafe1f0" }  # Amazon Linux 2 AMI
 
 resource "aws_instance" "flask_app" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  subnet_id              = var.subnet_id
-  user_data              = file("setup.sh")
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+
+  user_data = file("setup.sh")
 
   tags = {
-    Name = "FlaskAppInstance"
+    Name = "FlaskApp"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,36 +1,29 @@
-variable "aws_region" {
-  description = "The AWS region to deploy to"
-  type        = string
-  default     = "us-west-2"
-}
-
-variable "instance_type" {
-  description = "The EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "The name of the EC2 Key Pair"
-  type        = string
-  default     = "projectone"
-}
-
-variable "ami_id" {
-  description = "The AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-0648742c7600c103f"
-}
-
 variable "vpc_id" {
   description = "The VPC ID"
-  type        = string
   default     = "vpc-056766a49ea873983"
 }
 
 variable "subnet_id" {
-  description = "The ID of the subnet"
-  type        = string
+  description = "The Subnet ID"
   default     = "subnet-03d934629af7d951e"
 }
-  
+
+variable "security_group_id" {
+  description = "The Security Group ID"
+  default     = "sg-025f828e00022b542"
+}
+
+variable "key_name" {
+  description = "The key pair name"
+  default     = "projectone"
+}
+
+variable "instance_type" {
+  description = "The instance type"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID"
+  default     = "ami-0648742c7600c103f"
+}
