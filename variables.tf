bee390070a0c8bf5e85a0ef48e1ccf2663e8da15
Jason Gray <jasongray@Jasons-MacBook-Pro.local>
2024-07-28 01:17:40 -0700

    security group variable issue

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "subnet_id" {
   default     = "subnet-03d934629af7d951e"
 }
 
-variable "aws_security_group_id" {
+variable "avpc_security_group_ids" {
   description = "The security group ID"
   type        = string
   default     = "sg-025f828e00022b542"
