d1ecb2162fd164237335477bbf0f659970d456eb
Jason Gray <grayjason1970@gmail.com>
2024-07-27 23:26:12 -0700

    Create setup.sh
    setup file to install and initialize the flask application

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum update -y
+yum install -y git docker
+service docker start
+usermod -a -G docker ec2-user
+chkconfig docker on
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+# Clone the GitHub repo and run the Flask app
+git clone https://github.com/your-username/your-repo.git /home/ec2-user/app
+cd /home/ec2-user/app
+docker-compose up -d
